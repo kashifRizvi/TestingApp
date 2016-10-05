932ba61d554a6371945b7d8f0fcd113ff9e71f4c
Kashif Rizvi <srizvi@apostek.com>
2016-10-05 18:02:34 +0530

    Initial Commit

---
+++ TestingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestingApp
+//
+//  Created by Kashif on 05/10/16.
+//  Copyright © 2016 Kashif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestingApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestingApp
+//
+//  Created by Kashif on 05/10/16.
+//  Copyright © 2016 Kashif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
