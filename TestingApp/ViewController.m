68610839ddff35cd934a0df3625c8933990fd710
Kashif Rizvi <srizvi@apostek.com>
2016-10-14 18:12:10 +0530

    UIButton and UIImageView is working as Expected, Gesture for label not working as of yet.

--- TestingApp/ViewController.m
+++ TestingApp/ViewController.m
@@ -11,13 +11,13 @@
 @interface ViewController ()
 
 @end
-
+UIPanGestureRecognizer *panGesture;
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSInteger n=arc4random() % 9 + 1;
-    n=2;
+    n=1;
     
     switch (n) {
         case 1:
@@ -25,22 +25,31 @@
             break;
         case 2:
             [self showButton];
+            break;
         case 3:
             [self showImageView];
+            break;
         case 4:
             [self showView];
+            break;
         case 5:
             [self showSegmentedControl];
+            break;
         case 6:
             [self showTextField];
+            break;
         case 7:
             [self showSliderLabel];
+            break;
         case 8:
             [self showSwitch];
+            break;
         case 9:
             [self showActivityIndicator];
+            break;
         case 10:
             [self showProgressView];
+            break;
             
         default:
             break;
@@ -49,6 +58,22 @@
 
 - (void) showLabel{
     
+    UILabel *label=[[UILabel alloc]initWithFrame:CGRectMake(100.0, 100.0, 100.0, 20.0)];
+    [label setText:@"Drag me!!"];
+    [label setBackgroundColor:[UIColor cyanColor]];
+    [self.view addSubview:label];
+    panGesture=[[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(dragLabel:)];
+    panGesture.minimumNumberOfTouches = 1;
+    [label addGestureRecognizer:panGesture];
+}
+
+- (void) dragLabel: (UIPanGestureRecognizer *) gesture{
+    if (gesture.state==UIGestureRecognizerStateBegan) {
+        NSLog(@"Dragging began!!");
+    }
+    if (gesture.state==UIGestureRecognizerStateEnded) {
+        NSLog(@"Dragging Stopped!!");
+    }
 }
 
 - (void) showButton{
@@ -65,15 +90,30 @@
 }
 
 - (void) buttonClicked: (UIButton *) sender{
-    if (sender.state==UIGestureRecognizerStateBegan) {
-        UIAlertView *buttonAlert=[[UIAlertView alloc]initWithTitle:@"Button Pressed" message:@"Remove your finger from the Button." delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:nil];
+    if (sender.state == UIGestureRecognizerStateEnded) {
+        UIAlertView *buttonAlert=[[UIAlertView alloc]initWithTitle:@"Button Pressed" message:@"Remove your finger from the Button." delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil];
         [buttonAlert show];
-        
     }
 }
 
 - (void) showImageView{
     
+    UIImageView *imageView=[[UIImageView alloc]initWithFrame:CGRectMake(100.0, 100.0, 100.0, 100.0)];
+    imageView.image=[UIImage imageNamed:@"appleLogo.png"];
+    imageView.userInteractionEnabled = YES;
+    
+    UILongPressGestureRecognizer *longPressImage=[[UILongPressGestureRecognizer alloc]initWithTarget:self action:@selector(imagePressed:)];
+    [imageView addGestureRecognizer:longPressImage];
+    
+    [self.view addSubview:imageView];
+    
+}
+
+- (void) imagePressed: (UILongPressGestureRecognizer *) sender{
+    if (sender.state == UIGestureRecognizerStateBegan) {
+        UIAlertView *imageAlert=[[UIAlertView alloc]initWithTitle:@"Image Long Pressed" message:@"Try Pressing with multiple fingers." delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:nil];
+        [imageAlert show];
+    }
 }
 
 - (void) showView{
